f24207279ef1d204e3b6606280871d35940de24f
saralop4 <saralopezjb16@gmail.com>
2024-09-10 01:09:44 -0500

    mejoras del endpoint login

--- app/Presentation/Http/Controllers/AuthenticationController.php
+++ app/Presentation/Http/Controllers/AuthenticationController.php
@@ -2,9 +2,15 @@
 
 namespace App\Presentation\Http\Controllers;
 
+use Exception;
+use Illuminate\Http\Request;
 use App\Domain\DTOs\UsuarioDto;
-use App\Presentation\Http\Controllers\Controller;
+use App\Domain\Response\ApiResponse;
 use App\Application\Services\UsuarioService;
+use Illuminate\Validation\ValidationException;
+use App\Presentation\Http\Controllers\Controller;
+use Symfony\Component\Console\Output\ConsoleOutput;
+
 
 
 class AuthenticationController extends Controller{
@@ -16,11 +22,33 @@ class AuthenticationController extends Controller{
         $this->usuarioService = $usuarioService;
     }
 
-    public function login(UsuarioDto $usuarioDto)
+    public function login(Request $request)
     {
-        $response =  $this->usuarioService->loginUsuario($usuarioDto);
-        return $response;
+        $out = new ConsoleOutput();
+        $out->writeln("Hola desde la consola de Laravel");
+        try {
+
+            $out->writeln("entró");
+            $data = $request->only(['correo', 'contraseña']);
+
+            // Crear el DTO manualmente
+            $usuarioDto = new UsuarioDto();
+            $usuarioDto->setCorreo($data['correo']);
+            $usuarioDto->setContraseña($data['contraseña']);
+
+            $out->writeln("entró 2");
+
+
+            $response = $this->usuarioService->loginUsuario($usuarioDto);
+            return $response;
+
+        } catch (ValidationException ) {
+            return  ApiResponse::ResponseError('Error de validación', 422);
+        } catch (Exception ) {
+            return  ApiResponse::ResponseError('Error en el servidor', 500);
+        }
     }
+
 
     public function logout()
     {
@@ -38,6 +66,10 @@ class AuthenticationController extends Controller{
     {
         $response =  $this->usuarioService->refreshToken();
         return $response;
+    }
+
+    public function obtenerMensaje(){
+        return 'Hola desde el controller';
     }
 
 
--- routes/api.php
+++ routes/api.php
@@ -3,18 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Presentation\Http\Controllers\AuthenticationController;
 
-/*
-Route::prefix('v1')->group(function () {
-    Route::post('/iniciarSesion', [AuthenticationController::class, 'login']);
-    Route::post('/obtenerUsuarioActual', [AuthenticationController::class, 'me'])->middleware('auth:api');
-    Route::post('/cerrarSesion', [AuthenticationController::class, 'logout'])->middleware('auth:api');
-    Route::post('/refreshToken', [AuthenticationController::class, 'refreshToken'])->middleware('auth:api');
-    Route::get('/obtenerMensaje',function(){
-        return 'Hola Mundo';
-    });
-});
-*/
-
 Route::group([
     'middleware' => 'api',
     'prefix' => 'v1'
@@ -23,7 +11,5 @@ Route::group([
     Route::post('/logout', [AuthenticationController::class, 'logout'])->middleware('auth:api');
     Route::post('/refreshToken', [AuthenticationController::class, 'refreshToken'])->middleware('auth:api');
     Route::post('/me', [AuthenticationController::class, 'me'])->middleware('auth:api');
-     Route::get('/obtenerMensaje',function(){
-        return 'Hola Mundo';
-    });
+     Route::get('/obtenerMensaje', [AuthenticationController::class, 'obtenerMensaje']);
 });
